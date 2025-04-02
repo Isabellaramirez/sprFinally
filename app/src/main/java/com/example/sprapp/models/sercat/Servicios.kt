c7fb943d0671aad430b7b2015ba81b3906b737b2
isabella <isabella.ramirezgg@gmail.com>
2025-04-02 17:07:07 -0500

    Cambio de repositorio a isabella

--- app/src/main/java/com/example/sprapp/models/sercat/Servicios.kt
+++
@@ -1,8 +0,0 @@
-package main.models.sercat
-import com.example.sprapp.models.enums.trabajos
-import com.example.sprapp.models.enums.EnumCategoria
-data class Servicios(val id : String,
-                var nombreServicio : trabajos,
-                var descripcionServicio : String,
-                var cedulaPrestador: String,
-                var categoria: EnumCategoria)
