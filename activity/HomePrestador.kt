576d1f50ff550a9116d6227d00d64b0faf67f833
isabella <isabella.ramirezgg@gmail.com>
2025-04-23 07:19:14 -0500

    23/04/2025 7:18 am cambios en el activity home prestador y en el layout

--- activity/HomePrestador.kt
+++ activity/HomePrestador.kt
@@ -1,6 +1,12 @@
 package com.example.appsrp.activity
 
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,6 +22,29 @@ class HomePrestador : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        //obtener los datos del intent
+        val nombre = intent.getStringExtra("nombre") ?: "Nombre del Prestador"
+        val apellidos = intent.getStringExtra("apellidos") ?: ""
+        val fotoUri = intent.getStringExtra("fotoUri")
+
+        //configurar nombre
+        val nombreCompleto = "$nombre $apellidos"
+        findViewById<TextView>(R.id.nombreUsuario).text = nombreCompleto
+
+        //configuracion de la foto
+        fotoUri?.let {
+            try {
+                val bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P){
+                    ImageDecoder.decodeBitmap(ImageDecoder.createSource(contentResolver, Uri.parse(it)))
+                } else {
+                    MediaStore.Images.Media.getBitmap(contentResolver, Uri.parse(it))
+                }
+                findViewById<ImageView>(R.id.fotoPerfil).setImageBitmap(bitmap)
+            } catch (e: Exception){
+                e.printStackTrace()
+            }
         }
     }
 }
